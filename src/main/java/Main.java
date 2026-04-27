285e8f7504817b3aa6a0512ffee49cc855811a7d
sebastian <seguillen@csumb.edu>
2026-04-27 15:06:57 -0700

    converted village scene onto main. created inventory and fight button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,20 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 
 public class Main extends Application {
     private static final int SCENE_WIDTH = 430;
@@ -42,11 +52,35 @@ public class Main extends Application {
     @Override
     //main start of the program don't touch.
     public void start(Stage stage) {
+        Font.loadFont(getClass().getResourceAsStream("/org/turnbasedtitans/project2/fonts/PixelifySans.ttf"), 12);
         userDataManager = new DatabaseManager();
         userDAO = new UserDAO(userDataManager);
 
         stage.setTitle("Battle Quest");
-        stage.setScene(home(stage));
+        stage.setScene(town(stage));
+
+        // sets the screen resolution to always be 4:3, and to start off at a decent ish size on first boot (hopefully)
+        javafx.geometry.Rectangle2D screenBounds = javafx.stage.Screen.getPrimary().getVisualBounds();
+
+        double widthRatio = 1024.0 / 1512.0;
+        double heightRatio = 768.0 / 982.0;
+
+        double windowWidth = screenBounds.getWidth() * widthRatio;
+        double windowHeight = screenBounds.getHeight() * heightRatio;
+
+        stage.setWidth(windowWidth);
+        stage.setHeight(windowHeight);
+        stage.centerOnScreen();
+
+        double ratio = 4.0 / 3.0;
+
+        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
+            stage.setHeight(newVal.doubleValue() / ratio);
+        });
+
+        stage.heightProperty().addListener((obs, oldVal, newVal) -> {
+            stage.setWidth(newVal.doubleValue() * ratio);
+        });
         stage.show();
     }
 
@@ -95,8 +129,7 @@ public class Main extends Application {
             boolean created = userDAO.createUser(user, pass);
 
             if (created) {
-                result.setText("Account created successfully. this is here because town scene does not exist i will remove later");
-                //stage.setScene(town(stage));
+                stage.setScene(town(stage));
             } else {
                 result.setText("Username already exists.");
             }
@@ -135,8 +168,7 @@ public class Main extends Application {
             }
 
             if (userDAO.loginUser(user, pass)) {
-                result.setText("Logged in successfully. this is here because town scene does not exist I will remove later");
-                //stage.setScene(town(stage));
+                stage.setScene(town(stage));
             } else {
                 result.setText("Invalid username or password.");
             }
@@ -152,6 +184,159 @@ public class Main extends Application {
 
         return new Scene(general, SCENE_WIDTH, SCENE_HEIGHT);
     }
+    private Scene town(Stage stage) {
+        AnchorPane layout = new AnchorPane();
+        layout.setStyle("-fx-font-family: 'Pixelify Sans';");
+        Image bgImage = new Image(getClass().getResource("/org/turnbasedtitans/project2/village/village-bg.jpg").toExternalForm());
+        BackgroundImage bg = new BackgroundImage(
+                bgImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)
+        );
+        layout.setBackground(new Background(bg));
+
+        Label title = new Label("THE VILLAGE");
+        title.setStyle("-fx-font-size: 50px; -fx-font-weight: bold;");
+        title.setAlignment(Pos.CENTER);
+        title.setMaxWidth(Double.MAX_VALUE);
+        AnchorPane.setTopAnchor(title, 20.0);
+        AnchorPane.setLeftAnchor(title, 0.0);
+        AnchorPane.setRightAnchor(title, 0.0);
+
+        ProgressBar healthBar = new ProgressBar(1.0);
+        healthBar.setPrefHeight(25);
+        healthBar.setMaxWidth(Double.MAX_VALUE);
+        healthBar.setStyle("-fx-accent: #16d715;");
+        HBox.setHgrow(healthBar, Priority.ALWAYS);
+
+        Label healthLabel = new Label("100%");
+        healthLabel.setStyle("-fx-font-size: 20px;");
+
+        HBox healthContainer = new HBox(6, healthBar, healthLabel);
+        healthContainer.setAlignment(Pos.CENTER);
+        AnchorPane.setTopAnchor(healthContainer, 85.0);
+        AnchorPane.setLeftAnchor(healthContainer, 15.0);
+        AnchorPane.setRightAnchor(healthContainer, 15.0);
+
+        Button inventoryButton = shopButton("Inventory", "");
+        Button shopButton = shopButton("Shop", "");
+        Button fightButton = shopButton("Fight", "");
+
+        VBox mainPanel = new VBox(22, inventoryButton, shopButton, fightButton);
+        mainPanel.setAlignment(Pos.CENTER);
+        mainPanel.setPrefSize(350, 320);
+        mainPanel.setPadding(new Insets(28, 18, 28, 18));
+        mainPanel.setStyle("-fx-background-color: rgba(10, 73, 108, 0.75); -fx-border-color: #0A496C; -fx-border-width: 3px;");
+        AnchorPane.setTopAnchor(mainPanel, 220.0);
+        AnchorPane.setLeftAnchor(mainPanel, 520.0);
+
+        Label shopTitle = new Label("SHOP");
+        shopTitle.setStyle("-fx-font-size: 38px; -fx-text-fill: white;");
+
+        Label battlesText = new Label("Battle's Won -");
+        battlesText.setStyle("-fx-font-size: 26px; -fx-text-fill: white;");
+
+        Label battlesValue = new Label("0");
+        battlesValue.setStyle("-fx-font-size: 26px; -fx-text-fill: yellow;");
+
+        HBox battlesWonLabel = new HBox(8, battlesText, battlesValue);
+        battlesWonLabel.setAlignment(Pos.CENTER);
+
+        Button armorButton = shopButton("Armor Upgrade -", "1");
+        Button swordButton = shopButton("Sword Upgrade -", "2");
+        Button healingButton = shopButton("Healing -", "1");
+        Button backButton = shopButton("Back", "");
+
+        VBox shopPanel = new VBox(18, shopTitle, battlesWonLabel, armorButton, swordButton, healingButton, backButton);
+        shopPanel.setAlignment(Pos.TOP_CENTER);
+        shopPanel.setPrefSize(350, 420);
+        shopPanel.setMaxHeight(420);
+        shopPanel.setPadding(new Insets(28, 18, 18, 18));
+        shopPanel.setStyle("-fx-background-color: rgba(10, 73, 108, 0.75); -fx-border-color: #0A496C; -fx-border-width: 3px;");
+        AnchorPane.setTopAnchor(shopPanel, 160.0);
+        AnchorPane.setLeftAnchor(shopPanel, 520.0);
+        shopPanel.setVisible(false);
+
+        Label swordLabel = new Label("Sword: Bronze");
+        swordLabel.setStyle("-fx-font-size: 28px; -fx-text-fill: white;");
+
+        Label armorLabel = new Label("Armor: Bronze");
+        armorLabel.setStyle("-fx-font-size: 28px; -fx-text-fill: white;");
+
+        Label potionLabel = new Label("Healing Potions: 0");
+        potionLabel.setStyle("-fx-font-size: 28px; -fx-text-fill: white;");
+
+        Button inventoryBackButton = shopButton("Back", "");
+
+        javafx.scene.control.Separator sep1 = new javafx.scene.control.Separator();
+        javafx.scene.control.Separator sep2 = new javafx.scene.control.Separator();
+
+        VBox inventoryPanel = new VBox(12, swordLabel, sep1, armorLabel, sep2, potionLabel, inventoryBackButton);
+        inventoryPanel.setAlignment(Pos.TOP_LEFT);
+        inventoryPanel.setPrefSize(350, 360);
+        inventoryPanel.setPadding(new Insets(28, 18, 28, 18));
+        inventoryPanel.setStyle("-fx-background-color: rgba(10, 73, 108, 0.75); -fx-border-color: #0A496C; -fx-border-width: 3px;");
+        AnchorPane.setTopAnchor(inventoryPanel, 180.0);
+        AnchorPane.setLeftAnchor(inventoryPanel, 520.0);
+        inventoryPanel.setVisible(false);
+
+        inventoryButton.setOnAction(e -> {
+            mainPanel.setVisible(false);
+            inventoryPanel.setVisible(true);
+        });
+        shopButton.setOnAction(e -> {
+            mainPanel.setVisible(false);
+            shopPanel.setVisible(true);
+        });
+        fightButton.setOnAction(e -> stage.setScene(dungeonStart(stage)));
+        backButton.setOnAction(e -> {
+            shopPanel.setVisible(false);
+            mainPanel.setVisible(true);
+        });
+        inventoryBackButton.setOnAction(e -> {
+            inventoryPanel.setVisible(false);
+            mainPanel.setVisible(true);
+        });
+
+        layout.getChildren().addAll(title, healthContainer, mainPanel, shopPanel, inventoryPanel);
+        return new Scene(layout, SCENE_WIDTH, SCENE_HEIGHT);
+    }
+
+    private Button shopButton(String itemText, String priceText) {
+        Label itemLabel = new Label(itemText);
+        itemLabel.setStyle("-fx-font-size: 28px; -fx-text-fill: white;");
+
+        HBox buttonText;
+        if (priceText.isEmpty()) {
+            buttonText = new HBox(itemLabel);
+        } else {
+            Label priceLabel = new Label(priceText);
+            priceLabel.setStyle("-fx-font-size: 28px; -fx-text-fill: yellow;");
+            buttonText = new HBox(10, itemLabel, priceLabel);
+        }
+
+        buttonText.setAlignment(Pos.CENTER);
+
+        Button button = new Button();
+        button.setGraphic(buttonText);
+        button.setPrefHeight(85);
+        button.setMaxWidth(Double.MAX_VALUE);
+        String baseStyle = "-fx-background-color: #3C78D8; -fx-background-radius: 0; -fx-border-color: #0A496C; -fx-border-width: 3px; -fx-padding: 0;";
+        String hoverStyle = "-fx-background-color: #2E5FB8; -fx-background-radius: 0; -fx-border-color: #0A496C; -fx-border-width: 3px; -fx-padding: 0;";
+        String pressedStyle = "-fx-background-color: #244A91; -fx-background-radius: 0; -fx-border-color: #0A496C; -fx-border-width: 3px; -fx-padding: 0;";
+
+        button.setStyle(baseStyle);
+
+        button.setOnMouseEntered(e -> button.setStyle(hoverStyle));
+        button.setOnMouseExited(e -> button.setStyle(baseStyle));
+        button.setOnMousePressed(e -> button.setStyle(pressedStyle));
+        button.setOnMouseReleased(e -> button.setStyle(hoverStyle));
+        return button;
+    }
+
+
     private Scene dungeonStart (Stage stage) {
         int dungeonSpacing = 10;
 
@@ -163,7 +348,7 @@ public class Main extends Application {
         Button backButton = new Button(goBack);
         Button forwardButton = new Button(pressOn);
 
-        //backButton.setOnAction(e -> stage.setScene(town(stage));
+        backButton.setOnAction(e -> stage.setScene(town(stage)));
         forwardButton.setOnAction(e -> stage.setScene(dungeonFight(stage)));
 
         VBox titleSection = new VBox(dungeonSpacing, titleLabel);
@@ -182,6 +367,7 @@ public class Main extends Application {
         return new Scene(dungeonScene, SCENE_WIDTH, SCENE_HEIGHT);
     }
     private Scene dungeonFight (Stage stage) {
+
         //TEXT SECTION
         Label encounterLabel = new Label("A wild enemy appears!");
         Label warningLabel = new Label(encounterText);
