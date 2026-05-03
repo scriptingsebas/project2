334b61bfc6851270bc353fadde68dcf348da000c
IPabon05 <ipabon0516@gmail.com>
2026-05-02 22:02:22 -0700

    Within the RPGBattleSys Class, multiple additions have been made. Functionality for all buttons have been added.

--- src/main/java/RPGBattleSys.java
+++ src/main/java/RPGBattleSys.java
@@ -2,7 +2,9 @@ import java.util.Random;
 
 public class RPGBattleSys {
     //PRESET(S)
+    private int playerHP = 100;
     private final int defaultDamage = 10;
+    private boolean defendState = false;
     private final Random RNG = new Random();
     private Enemies dungeonEnemy;
 
@@ -10,11 +12,34 @@ public class RPGBattleSys {
     public RPGBattleSys(Enemies dungeonEnemy) {
         this.dungeonEnemy = dungeonEnemy;
     }
-
+    //ATTACK FORMULA FOR WHEN THE PLAYER PRESSES "ATTACK"
     public int attackEnemy() {
         int diceDamage = RNG.nextInt(20) + 1;
         int finalDamage = defaultDamage + diceDamage;
         dungeonEnemy.dmgTaken(finalDamage);
         return finalDamage;
     }
+    //DEFEND FUNCTIONALITY - SETS "defendState" TO TRUE
+    public void activateDefend() {
+        defendState = true;
+    }
+    //ATTACK FORMULA FOR WHEN THE ENEMY "ATTACKS" THE PLAYER
+    public int attackUser() {
+        int enemyDealt = RNG.nextInt(defaultDamage) + 1;
+        if(defendState) {
+            enemyDealt /= 2;
+            defendState = false;
+        }
+        playerHP -= enemyDealt;
+        return enemyDealt;
+    }
+    //ESCAPE FUNCTIONALITY FOR WHEN THE PLAYER PRESSES "ESCAPE"
+    public int escapeRNG() {
+        return RNG.nextInt(6) + 1;
+    }
+    //ESCAPE SUCCESS
+    public boolean escapeYayNay(int diceRoll) {
+        return diceRoll >= 3;
+    }
+
 }
